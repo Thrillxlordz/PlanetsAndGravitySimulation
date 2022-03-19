708cf02359d67ccc39beb45cccc973f8147930ae
Nick McCord <nickjmccord@gmail.com>
2022-03-19 19:18:27 -0400

    Nearly Finished Product! Still a known bug with hard collisions

--- script.js
+++ script.js
@@ -1,107 +1,201 @@
 let canvasX
 let canvasY
-let lines = []
-let numLines = 12
-let speed = 15
-let mouseBias
-let maxPoints = 150
-let maxWeight = 5
+let backgroundColor
+let stars
+let planets
+let numPlanets = 10
+let maxRadius = 45
+let minRadius = 15
+let minBlackness = 50
+let planetHit
+let myRocket
+let dt = 0.02
+let mouseDown = false
+let clickedLocation
 
 function setup() {
   canvasX = windowWidth * 0.95
-  canvasY = windowHeight * 0.8
-  myCanvas = createCanvas(canvasX, canvasY)
-  myCanvas.parent('canvas')
-  background(0)
-  noFill()
+  canvasY = windowHeight * 0.84
+  let myCanvas = createCanvas(canvasX, canvasY)
+  myCanvas.parent("#canvas")
+  backgroundColor = window.getComputedStyle(document.getElementById("canvas")).getPropertyValue('background-color')
+  background(backgroundColor)
+
+  stars = []
+  numStars = canvasX * canvasY / 1000
+  for (let i = 0; i < numStars; i++) {
+    stars.push(new star())
+    stars[i].radius = random(2)
+    stars[i].center = createVector(random(canvasX), random(canvasY))
+    stars[i].color = color(200 + random(50), 200 + random(50), 200 + random(50))
+  }
+
+  planets = []
+  for (let i = 0; i < numPlanets; i++) {
+    planets.push(new planet())
+    planets[i].radius = ceil(minRadius + random(maxRadius - minRadius))
+    planets[i].center = findValidSpot(planets[i])
+    planets[i].mass = pow(planets[i].radius, 3)
+    planets[i].color = color(minBlackness + random(250 - minBlackness), minBlackness + random(250 - minBlackness), minBlackness + random(250 - minBlackness))
+  }
+  myRocket = new rocket()
+  myRocket.radius = 5
+  myRocket.location = findValidSpot(myRocket)
+  myRocket.velocity = createVector(0, 0)
+  myRocket.acceleration = createVector(0, 0)
 }
 
 function draw() {
-  if (mouseX > 0 && mouseX < canvasX && mouseY > 0 && mouseY < canvasY) {
-    mouseBias = true
+  background(backgroundColor)
+
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].draw()
+  }
+
+  for (let i = 0; i < planets.length; i++) {
+    planets[i].draw()
+  }
+
+  myRocket.draw()
+
+  if (mouseDown) {
+    if (!collidesWithPlanet(clickedLocation.x, clickedLocation.y, myRocket.radius)) {
+      stroke(color(255, 0, 0))
+      line(myRocket.location.x, myRocket.location.y, mouseX, mouseY)
+    }
+    return
+  }
+
+  strokeWeight(2)
+  stroke(color(255, 0, 0))
+  let dir = myRocket.velocity.copy()
+  dir.normalize()
+  dir.mult(myRocket.radius)
+  line(myRocket.location.x, myRocket.location.y, myRocket.location.x + dir.x, myRocket.location.y + dir.y)
+
+  myRocket.move()
+  if (collidesWithPlanet(myRocket.location.x, myRocket.location.y, myRocket.radius)) {
+    myRocket.bounceOff(planetHit.center, planetHit.radius)
+  }
+}
+
+function mousePressed() {
+  mouseDown = true
+  clickedLocation = createVector(mouseX, mouseY)
+  if (collidesWithPlanet(clickedLocation.x, clickedLocation.y, myRocket.radius)) {
+    return
+  }
+  myRocket.location = createVector(mouseX, mouseY)
+}
+
+function mouseClicked() {
+  mouseDown = false
+  newVel = createVector(clickedLocation.x - mouseX, clickedLocation.y - mouseY)
+  myRocket.velocity = newVel
+}
+
+function findValidSpot(obj) {
+  let centerX = obj.radius + random(canvasX - obj.radius * 2)
+  let centerY = obj.radius + random(canvasY - obj.radius * 2)
+  while (collidesWithPlanet(centerX, centerY, obj.radius)) {
+    centerX = obj.radius + random(canvasX - obj.radius * 2)
+    centerY = obj.radius + random(canvasY - obj.radius * 2)
+  }
+  return createVector(centerX, centerY)
+}
+
+function collidesWithPlanet(x, y, radius) {
+  let planetCount
+  if (radius >= minRadius) {
+    planetCount = planets.length - 1
   } else {
-    mouseBias = false
+    planetCount = planets.length
   }
-  background(0)
-  let x, y, offScreen
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i].points.length > maxPoints) {
-      lines[i].points.splice(0, 1)
+  for (let i = 0; i < planetCount; i++) {
+    if (dist(x, y, planets[i].center.x, planets[i].center.y) <= planets[i].radius + radius) {
+      planetHit = planets[i]
+      return true
     }
-    stroke(lines[i].color)
-    strokeWeight(lines[i].weight)
-    x = lines[i].points[lines[i].points.length - 1].x
-    y = lines[i].points[lines[i].points.length - 1].y
-    let randX = random(speed * 2) - speed
-    let randY = random(speed * 2) - speed
-    if (mouseBias) {
-      for (let j = 0; j < 2; j++) {
-        let rx = random(speed * 2) - speed
-        let ry = random(speed * 2) - speed
-        if (dist(x + rx, y + ry, mouseX, mouseY) < dist(x + randX, y + randY, mouseX, mouseY)) {
-          randX = rx
-          randY = ry
-        }
-      }
-    }
-    x += randX
-    y += randY
-    offScreen = false
-    if (x > canvasX) {
-      x -= canvasX
-      offScreen = true
-    } else if (x < 0) {
-      x += canvasX
-      offScreen = true
-    }
-    if (y > canvasY) {
-      y -= canvasY
-      offScreen = true
-    } else if (y < 0) {
-      y += canvasY
-      offScreen = true
-    }
-
-    lines[i].points.push(createVector(x, y))
-
-    beginShape()
-    vertex(lines[i].points[0].x, lines[i].points[0].y)
-    for (let j = 1; j < lines[i].points.length; j++) {
-      if (abs(lines[i].points[j].x - lines[i].points[j - 1].x) > speed) {
-        endShape()
-        beginShape()
-        continue
-      }
-      if (abs(lines[i].points[j].y - lines[i].points[j - 1].y) > speed) {
-        endShape()
-        beginShape()
-        continue
-      }
-      vertex(lines[i].points[j].x, lines[i].points[j].y)
-    }
-    endShape()
   }
+
+  return false
 }
 
-function createLine(x, y) {
-  lines.push(new line())
-  lines[lines.length - 1].points.push(createVector(x, y))
-  lines[lines.length - 1].color = color(random(255), random(255), random(255))
-  let weight = ceil(random(maxWeight))
-  while (weight % 2 == 0) {
-    weight = ceil(random(maxWeight))
-  }
-  lines[lines.length - 1].weight = ceil(random(maxWeight))
-
-    if (lines.length > numLines) {
-      lines.splice(0, 1)
-    }
-}
-
-class line {
-  constructor(points = [], color, weight) {
-    this.points = points
+class star {
+  constructor(center = 0, radius = 0, color = 0) {
+    this.center = center
+    this.radius = radius
     this.color = color
-    this.weight = weight
+    this.draw = function() {
+      noStroke()
+      fill(this.color)
+      ellipse(this.center.x, this.center.y, this.radius * 2)
+    }
+  }
+}
+
+class planet {
+  constructor(center = 0, radius = 1, mass = 5, color = 0) {
+    this.center = center
+    this.radius = radius
+    this.mass = mass
+    this.color = color
+    this.draw = function() {
+      noStroke()
+      fill(this.color)
+      ellipse(this.center.x, this.center.y, this.radius * 2)
+    }
+    this.pull = function(x, y) {
+      let dir = createVector(this.center.x - x, this.center.y - y)
+      dir.normalize()
+      dir.mult(this.mass / pow(dist(x, y, this.center.x, this.center.y), 2))
+      dir.mult(4 / 3 * PI)
+      return dir
+    }
+  }
+}
+
+class rocket {
+  constructor(location = 0, velocity = 0, acceleration = 0, radius = 1) {
+    this.location = location
+    this.velocity = velocity
+    this.acceleration = acceleration
+    this.radius = radius
+    this.draw = function() {
+      stroke(color(150, 150, 150))
+      strokeWeight(1)
+      fill(color(0, 200, 200))
+      ellipse(this.location.x, this.location.y, this.radius * 2)
+    }
+    this.move = function() {
+      if (this.location.x + this.velocity.x * dt < this.radius || this.location.x + this.velocity.x * dt > canvasX - this.radius) {
+        this.bounceOff(createVector((abs(this.velocity.x) / this.velocity.x) * canvasX * 2, this.location.y), 0)
+      }
+      if (this.location.y + this.velocity.y * dt < this.radius || this.location.y + this.velocity.y * dt > canvasY - this.radius) {
+        this.bounceOff(createVector(this.location.x, (abs(this.velocity.y) / this.velocity.y) * canvasY * 2), 0)
+      }
+      this.location.x += this.velocity.x * dt
+      this.location.y += this.velocity.y * dt
+      this.velocity.x += this.acceleration.x * dt
+      this.velocity.y += this.acceleration.y * dt
+      let accel = createVector(0,0)
+      for (let i = 0; i < planets.length; i++) {
+        accel.add(planets[i].pull(this.location.x, this.location.y))
+      }
+      this.acceleration.x = accel.x
+      this.acceleration.y = accel.y
+    }
+    this.bounceOff = function(objCenter, objRadius) {
+      if (this.velocity.mag() < this.radius) {
+        console.log("crash landing!")
+      }
+      let dir = createVector(0,0)
+      dir.x = objCenter.x
+      dir.y = objCenter.y
+      dir.sub(this.location)
+      dir.normalize()
+      this.velocity.reflect(dir)
+      this.velocity.mult(0.8)
+    }
   }
 }
